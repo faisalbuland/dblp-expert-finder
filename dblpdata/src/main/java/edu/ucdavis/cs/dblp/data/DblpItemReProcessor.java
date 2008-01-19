907fe6f85bb932c5ddecaab4cd2f9b6a6188b998
pfishero <pfishero@1649a38b-c83f-0410-9125-a9322e9dffef>
2008-01-19 17:18:32 +0000

    added some debug logging

--- dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/DblpItemReProcessor.java
+++ dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/DblpItemReProcessor.java
@@ -1,11 +1,19 @@
 package edu.ucdavis.cs.dblp.data;
 
+import org.apache.commons.lang.StringUtils;
+
 public class DblpItemReProcessor extends DblpItemProcessor {
 	@Override
 	protected void processItem(Object entry) {
 		Publication pub = Publication.convert(entry);
+		logger.debug("processing pub key="+pub.getKey()+
+				" cites:"+StringUtils.join(pub.getCite(), ", "));
 			
 		Publication persistedPub = dao.findById(pub.getKey());
+		if (null != persistedPub) {
+			logger.debug("persistedpub key="+pub.getKey()+
+				" cites:"+StringUtils.join(pub.getCite(), ", "));
+		}
 		if (null == persistedPub) {
 			// not found in the DB
 			contentService.retrieveAll(pub);
@@ -14,11 +22,11 @@ public class DblpItemReProcessor extends DblpItemProcessor {
 			dao.update(pub);
 		} else if (null == persistedPub.getContent()) {
 			contentService.retrieveAll(pub);
-			logger.debug("was missing content: "+pub.getKey() + ' ' +
+			logger.info("was missing content: "+pub.getKey() + ' ' +
 					(pub.getContent() == null?"no content": "has content"));
 			dao.update(pub);
 		} else {
-			logger.debug("skipping persisted pub with content "+pub.getKey());
+			logger.info("skipping persisted pub with content "+pub.getKey());
 		}
 	}
 }
