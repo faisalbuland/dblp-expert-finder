1c77106d485a3abbbb14cfbecedaccfc4e523331
pfishero <pfishero@1649a38b-c83f-0410-9125-a9322e9dffef>
2008-01-22 09:31:19 +0000

    Added logic to filter out bad cites entries ("..." ones).

--- dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/DblpItemReProcessor.java
+++ dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/DblpItemReProcessor.java
@@ -1,5 +1,11 @@
 package edu.ucdavis.cs.dblp.data;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.Sets;
+
+import de.unitrier.dblp.Cite;
+
 
 public class DblpItemReProcessor extends DblpItemProcessor {
 	@Override
@@ -12,15 +18,33 @@ public class DblpItemReProcessor extends DblpItemProcessor {
 			contentService.retrieveAll(pub);
 			logger.info("not found in DB: "+pub.getKey() + ' ' +
 					(pub.getContent() == null?"no content": "has content"));
+			filterBadCites(pub);
 			dao.update(pub);
 		} else if (null == persistedPub.getContent()) {
 			// found in the DB, but has no content
 			contentService.retrieveAll(persistedPub);
 			logger.info("was missing content: "+persistedPub.getKey() + ' ' +
 					(persistedPub.getContent() == null?"no content": "has content"));
+			filterBadCites(persistedPub);
 			dao.update(persistedPub);
 		} else { // found in the DB with content
 			logger.info("skipping persisted pub with content "+pub.getKey());
+		}
+	}
+	
+	/**
+	 * Filters out Cite entries that are empty (e.g. "..." as their value
+	 * and have no label).
+	 * 
+	 * @param pub
+	 */
+	private void filterBadCites(Publication pub) {
+		for (Cite cite : Sets.immutableSet(pub.getCite())) {
+			if ("...".equals(cite.getContent().trim()) && 
+					StringUtils.isBlank(cite.getLabel())) {
+				pub.getCite().remove(cite);
+				logger.info("removed empty ... cite from "+pub.getKey());
+			}
 		}
 	}
 }
