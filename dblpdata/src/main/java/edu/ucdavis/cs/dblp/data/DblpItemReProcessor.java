cc412793b28194f95c37dec27179a2ef86bb1c92
pfishero <pfishero@1649a38b-c83f-0410-9125-a9322e9dffef>
2008-01-23 09:44:41 +0000

    A more robust version of the re-processor.  Currently it is set in a "output logs on what is and is not in the DB" mode.

--- dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/DblpItemReProcessor.java
+++ dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/DblpItemReProcessor.java
@@ -1,6 +1,7 @@
 package edu.ucdavis.cs.dblp.data;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
 import com.google.common.collect.Sets;
 
@@ -8,27 +9,58 @@ import de.unitrier.dblp.Cite;
 
 
 public class DblpItemReProcessor extends DblpItemProcessor {
+	public static final Logger logger = Logger.getLogger(DblpItemReProcessor.class);
+	
+	/**
+	 * PROCESS==true means re-process the data - download if needed and then save via the dao
+	 * PROCESS==false means just output the state of the item (in the db or not, has content or not)
+	 */
+	private static final boolean PROCESS=false;
+	
 	@Override
 	protected void processItem(Object entry) {
 		Publication pub = Publication.convert(entry);
+		
+		try {
+			Publication persistedPub = dao.findById(pub.getKey());
 			
-		Publication persistedPub = dao.findById(pub.getKey());
-		if (null == persistedPub) {
-			// not found in the DB
-			contentService.retrieveAll(pub);
-			logger.info("not found in DB: "+pub.getKey() + ' ' +
-					(pub.getContent() == null?"no content": "has content"));
-			filterBadCites(pub);
-			dao.update(pub);
-		} else if (null == persistedPub.getContent()) {
-			// found in the DB, but has no content
-			contentService.retrieveAll(persistedPub);
-			logger.info("was missing content: "+persistedPub.getKey() + ' ' +
-					(persistedPub.getContent() == null?"no content": "has content"));
-			filterBadCites(persistedPub);
-			dao.update(persistedPub);
-		} else { // found in the DB with content
-			logger.info("skipping persisted pub with content "+pub.getKey());
+			if (null == persistedPub) {
+				// not found in the DB
+				if (PROCESS) {
+					contentService.retrieveAll(pub);
+					logger.info("not found in DB: "+pub.getKey() + ' ' +
+							(pub.getContent() == null?"no content": "has content"));
+					filterBadCites(pub);
+					dao.update(pub);
+				} else {
+					logger.info("not found in DB: "+pub.getKey() + 
+							" [ee]:"+pub.getEe());
+					// insert without content, just to have in the DB
+					filterBadCites(pub);
+					dao.update(pub);
+				}
+			} else if (null == persistedPub.getContent()) {
+				if (PROCESS) {
+					// found in the DB, but has no content
+					contentService.retrieveAll(persistedPub);
+					logger.info("was missing content: "+persistedPub.getKey() + ' ' +
+							(persistedPub.getContent() == null?"no content": "has content"));
+					filterBadCites(persistedPub);
+					dao.update(persistedPub);
+				} else {
+					logger.info("was missing content: " + 
+							persistedPub.getKey() +" [ee]:"+persistedPub.getEe());
+				}
+			} else { // found in the DB with content
+				logger.info("skipping persisted pub with content "+pub.getKey());
+			}
+		} catch (org.hibernate.exception.ConstraintViolationException cve) {
+			// explicitly catching this error until a resolution can be found
+			logger.error("cve error:"+cve);
+			logger.error("cve error pub:"+pub.getKey()+" ee:"+pub.getEe());
+			logger.error("cve error cites:"+StringUtils.join(pub.getCite(), ", "));
+		} catch (RuntimeException rex) {
+			logger.error("error while processing pub="+pub.getKey()+'-'+rex);
 		}
 	}
 	
@@ -43,7 +75,9 @@ public class DblpItemReProcessor extends DblpItemProcessor {
 			if ("...".equals(cite.getContent().trim()) && 
 					StringUtils.isBlank(cite.getLabel())) {
 				pub.getCite().remove(cite);
-				logger.info("removed empty ... cite from "+pub.getKey());
+				logger.info("removed empty ... cite from "+pub.getKey() + 
+						" remaining cites:" + 
+						StringUtils.join(pub.getCite(), ", "));
 			}
 		}
 	}
