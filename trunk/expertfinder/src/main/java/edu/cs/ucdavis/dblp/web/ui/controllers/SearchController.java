4017cac63e11f9b783ae8d438031431dc57de156
pfishero <pfishero@1649a38b-c83f-0410-9125-a9322e9dffef>
2008-02-25 10:30:33 +0000

    Browse by Researcher support.

--- trunk/expertfinder/src/main/java/edu/cs/ucdavis/dblp/web/ui/controllers/SearchController.java
+++ trunk/expertfinder/src/main/java/edu/cs/ucdavis/dblp/web/ui/controllers/SearchController.java
@@ -4,6 +4,7 @@
 package edu.cs.ucdavis.dblp.web.ui.controllers;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import javax.faces.component.UIComponent;
 
@@ -14,6 +15,8 @@ import com.google.common.collect.Lists;
 import de.unitrier.dblp.Author;
 import edu.ucdavis.cs.dblp.ServiceLocator;
 import edu.ucdavis.cs.dblp.experts.ResearcherDao;
+import edu.ucdavis.cs.dblp.experts.ResearcherProfile;
+import edu.ucdavis.cs.dblp.experts.ResearcherProfileImpl;
 
 /**
  * Controller for search related actions and state.
@@ -24,8 +27,12 @@ public class SearchController {
 	public static final Logger logger = Logger.getLogger(SearchController.class);
 	
 	private String searchText;
+	private String researcherName;
 	private Collection<Author> authors;
-	private UIComponent resultsForm;
+	private ResearcherProfile profile;
+	private UIComponent authorSelector;
+	private UIComponent researcherInfo;
+	private UIComponent publicationsList;
 	
 	public SearchController() {
 		authors = Lists.newLinkedList();
@@ -39,35 +46,82 @@ public class SearchController {
 		this.searchText = searchText;
 	}
 	
-	public UIComponent getResultsForm() {
-		return resultsForm;
+	public String getResearcherName() {
+		return researcherName;
 	}
 
-	public void setResultsForm(UIComponent resultsForm) {
-		this.resultsForm = resultsForm;
+	public void setResearcherName(String researcherName) {
+		this.researcherName = researcherName;
 	}
-	
+
 	public Collection<Author> getAuthors() {
 		return authors;
 	}
 
 	public String authorSearch() {
-		authors.clear();
 		logger.info("searching for author with name: "+searchText);
+		doAuthorSearch(searchText);
 		
-		ResearcherDao dao = ServiceLocator.getInstance().getResearcherDao();
-		Collection<Author> authorResults = dao.findByNamePrefix(searchText);
-		logger.info("found "+authorResults.size()+" authors for prefix text: "+searchText);
-		this.authors = authorResults;
-
-	/*	for (String name : new String[]{"Michael Gertz", "Ian Davidson", "Test Author"}) {
-			Author auth = new Author();
-			auth.setContent(name);
-			authors.add(auth);
-		}*/
-
-		resultsForm.setRendered(authors.size() > 0);
+		authorSelector.setRendered(true);
+		researcherInfo.setRendered(false);
+		publicationsList.setRendered(false);
 		
 		return null;
+	}
+	
+	public String buildResearcherProfile() {
+		doAuthorSearch(researcherName);
+		this.searchText=researcherName;
+		
+		authorSelector.setRendered(false);
+		researcherInfo.setRendered(true);
+		publicationsList.setRendered(true);
+		
+		return null;
+	}
+	
+	private final void doAuthorSearch(String authorName) {
+		ResearcherDao dao = ServiceLocator.getInstance().getResearcherDao();
+		Collection<Author> authorResults = dao.findByNamePrefix(authorName);
+		logger.info("found "+authorResults.size()+" authors for name: "+authorName);
+		this.authors = authorResults;
+
+		setProfile(new ResearcherProfileImpl(authorResults.iterator().next()));		
+	}
+	
+	public ResearcherProfile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(ResearcherProfile profile) {
+		this.profile = profile;
+	}
+	
+	public UIComponent getAuthorSelector() {
+		return authorSelector;
+	}
+
+	public void setAuthorSelector(UIComponent authorSelector) {
+		this.authorSelector = authorSelector;
+	}
+
+	public UIComponent getResearcherInfo() {
+		return researcherInfo;
+	}
+
+	public void setResearcherInfo(UIComponent researcherInfo) {
+		this.researcherInfo = researcherInfo;
+	}
+
+	public UIComponent getPublicationsList() {
+		return publicationsList;
+	}
+
+	public void setPublicationsList(UIComponent publicationsList) {
+		this.publicationsList = publicationsList;
+	}
+	
+	public int getPublicationsCount() {
+		return this.profile == null ? 0 : this.profile.getPublications().size();
 	}
 }
