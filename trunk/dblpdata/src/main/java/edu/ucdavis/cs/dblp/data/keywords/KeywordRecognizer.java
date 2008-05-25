50a89a7aabb803b876c540a9aeb1d07347198852
pfishero <pfishero@1649a38b-c83f-0410-9125-a9322e9dffef>
2008-05-25 06:36:02 +0000

    Minor change to DAO. Added functions/support for extracting keywords from text.

--- trunk/dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/keywords/KeywordRecognizer.java
+++ trunk/dblpdata/src/main/java/edu/ucdavis/cs/dblp/data/keywords/KeywordRecognizer.java
@@ -3,6 +3,8 @@ package edu.ucdavis.cs.dblp.data.keywords;
 import java.util.List;
 import java.util.Set;
 
+import com.google.common.collect.BiMap;
+
 import edu.ucdavis.cs.dblp.data.Keyword;
 
 public interface KeywordRecognizer {
@@ -16,4 +18,13 @@ public interface KeywordRecognizer {
 	 * @return the reduced keywords
 	 */
 	List<String> reduceKeywords(List<String> keywords);
+	
+	List<Keyword> removeLowInformationKeywords(Iterable<Keyword> keywords);
+	
+	/**
+	 * Optional method to implement.  If implemented it will return a map from keywords
+	 * that are acronyms to their expanded form.  Example: WWW &lt;-&gt; World Wide Web. 
+	 * @return the acronym map (acronyms &lt;-&gt; expanded form)
+	 */
+	BiMap<String, String> getAcronymMap();
 }
