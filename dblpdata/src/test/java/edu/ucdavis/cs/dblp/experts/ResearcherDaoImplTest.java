5eb06b8e4abea7a465c6ed742020a835b8e39928
pfishero <pfishero@1649a38b-c83f-0410-9125-a9322e9dffef>
2008-06-17 09:26:29 +0000

    cluto clustering code

--- dblpdata/src/test/java/edu/ucdavis/cs/dblp/experts/ResearcherDaoImplTest.java
+++ dblpdata/src/test/java/edu/ucdavis/cs/dblp/experts/ResearcherDaoImplTest.java
@@ -3,11 +3,14 @@
  */
 package edu.ucdavis.cs.dblp.experts;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -15,13 +18,19 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.google.common.base.Join;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
 
 import de.unitrier.dblp.Author;
 import edu.ucdavis.cs.dblp.ServiceLocator;
 import edu.ucdavis.cs.dblp.data.Keyword;
+import edu.ucdavis.cs.dblp.data.Publication;
 import edu.ucdavis.cs.dblp.data.timeline.EventData;
+import edu.ucdavis.cs.dblp.text.ClutoClusterer;
+import edu.ucdavis.cs.dblp.text.SimplePub;
 
 /**
  * Tests the ResearcherDaoImpl.  Note: this requires the data store
@@ -96,12 +105,28 @@ public class ResearcherDaoImplTest {
 	@Test
 	public final void verifyAuthorProfile() {
 //		final String researcherName = "Ian Davidson";
-//		final String researcherName = "Shyhtsun Felix Wu";
-		final String researcherName = "Michael Gertz";
+		final String researcherName = "Shyhtsun Felix Wu";
+//		final String researcherName = "Michael Gertz";
 		ResearcherProfile profile = Iterables.getOnlyElement(dao.buildProfile(researcherName));
 		assertTrue(profile.getPublications().size() > 0);
 		List<Keyword> expertiseAreas = profile.identifyExpertiseAreas();
 		assertThat(expertiseAreas.size(), is(greaterThanOrEqualTo(1)));
+		
+		final int numClusters = 8;
+		Multimap<Integer, Publication> clusters = 
+							ClutoClusterer.getClustersFor(profile.getPublications(), numClusters);
+		assertThat(clusters.keySet().size(), is(numClusters));
+		for(int clusterId : clusters.keySet()) {
+			logger.info("\ncluster "+clusterId+" members:");
+			for (Publication pub : clusters.get(clusterId)) {
+				logger.info(pub.getTitle());
+			}
+			logger.info("\nkeywords:");
+			logger.info(
+					Join.join("\n", Join.join(", ", 
+							Iterables.partition(profile.simpleTopKeyphrases(
+									SimplePub.fromAll(clusters.get(clusterId))), 10, false))));
+		}
 	}
 
 }
